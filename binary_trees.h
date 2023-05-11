4d92be5ce8d16f15c9d46bca6e2fea6bb099ab62
Johnatan Ortiz <6403@holbertonstudents.com>
2023-05-11 08:52:08 -0700

    First commit, added README.md empty, and point 0 about add node in binary tree, it running, betty style tested

---
+++ binary_trees.h
@@ -0,0 +1,41 @@
+#ifndef _binary_trees_h
+#define _binary_trees_h
+
+/* LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* basic binary tree */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* binary search tree */
+typedef struct binary_tree_s bst_t;
+
+/* avl tree */
+
+typedef struct binary_tree_s avl_t;
+
+/* max binary heap */
+typedef struct binary_tree_s heap_t;
+
+/* prototypes */
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
